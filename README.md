df271ed6b11e817519d56a7c77c765216a11b9c8
Sanjeet <sanjeet.kumar@zylotech.com>
2018-05-01 15:45:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# flask-mongodb-rest-api
+A simple REST Api using Flask-Restful and MongoDB, used to store tweets with timestamps and associated emotions. The Api supports `get`, `put`, `post` and `delete` functions and has multiple endpoints for more complicated `get` requests.
+
+
+
+### Install requirements: ###
+Install the requirements in a virtual environment.
+```
+pip install -r requirements.txt
+```
+### API: ###
+```
+http://127.0.0.1:5000/api/v1/todo/tasks
+http://127.0.0.1:5000/api/v1/todo/tasks/<_id>
+```
---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify, url_for, redirect, request  # For flask implementation
+from flask_pymongo import PyMongo
+from flask_restful import Api, Resource
+from bson.objectid import ObjectId  # For ObjectId to work
+
+app = Flask(__name__)
+api = Api(app)
+
+# mongodb config
+app.config['MONGO_DBNAME'] = 'restdb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/restdb'
+mongo = PyMongo(app)
+
+# resource api
+class TaskListAPI(Resource):
+
+  def get(self):
+    task = mongo.db.tasks
+    output = []
+    for s in task.find():
+        output.append({'_id': str(s['_id']), 'title' : s['title'], 'description' : s['description']})
+    return jsonify({'result' : output})
+
+  def post(self):
+    task = mongo.db.tasks
+    title = request.json['title']
+    description = request.json['description']
+    task_id = task.insert({'title': title, 'description': description})
+    new_task = task.find_one({'_id': task_id })
+    output = {'title' : new_task['title'], 'description' : new_task['description']}
+    return jsonify({'result' : output})
+
+class TaskAPI(Resource):
+    
+  def get(self, task_id):
+    task = mongo.db.tasks
+    new_task = task.find_one({ '_id': ObjectId(task_id) })
+    output = {'_id': str(new_task['_id']), 'title' : new_task['title'], 'description' : new_task['description']}
+    return jsonify({'result' : output})
+
+  def put(self, task_id):    
+    title = request.json['title']
+    data = {'title': title}   
+    mongo.db.tasks.update({'_id': ObjectId(task_id)}, {'$set': data})    
+    return jsonify({'result' : 'task has been updated'})
+
+  def delete(self, task_id):
+    mongo.db.tasks.remove({ 'id': ObjectId(task_id) })
+    return jsonify({'result' : 'task has been deleted'})
+
+# add resource to endpoints
+api.add_resource(TaskListAPI, '/api/v1/todo/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/api/v1/todo/tasks/<string:task_id>', endpoint='task')
+
+# starting point config
+if __name__ == '__main__':
+	app.run(debug=True)
+
+			
